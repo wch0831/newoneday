55b041a1ea30e5a91814501c09439085ec5aef40
이용재 <@218.156.29.135>
2018-12-22 17:35:03 +0900

    faq 수정

--- oneday_project/src/com/oneday/faq/FaqServlet.java
+++ oneday_project/src/com/oneday/faq/FaqServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-import com.oneday.review.ReviewDAO;
-import com.oneday.review.ReviewVO;
 
 /**
  * Servlet implementation class FaqServlet
