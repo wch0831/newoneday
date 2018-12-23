9721eda160de51f2332fb3909b6c91c8c1ad86d4
TK <TK@DESKTOP-KHL6IBC>
2018-12-23 23:33:55 +0900

    dd

---
+++ oneday_project/src/com/oneday/place/PlaceMainDAO.java
@@ -0,0 +1,24 @@
+package com.oneday.place;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.oneday.common.MyBatisFactory;
+
+public class PlaceMainDAO {
+	public ArrayList<PlaceVO> mainSelect() {
+		ArrayList<PlaceVO> mainList = new ArrayList<PlaceVO>(); 
+		SqlSession conn =null;
+		try { 
+			conn = MyBatisFactory.getFactory().openSession();
+			mainList = (ArrayList)conn.selectList("placeNameSpace.mainSelect");
+		} finally {
+			conn.close();
+		}
+		return mainList;
+	}
+	
+	
+	
+}
