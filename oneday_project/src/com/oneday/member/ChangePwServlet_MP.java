936dba81354bf2b73b33a6513b01b01bc3ad1b71
KDH2 <KDH2@DESKTOP-7F6PI5O>
2018-12-25 21:23:43 +0900

    패스워스변경(마이페이지) 추가완성

--- oneday_project/src/com/oneday/member/ChangePwServlet_MP.java
+++ oneday_project/src/com/oneday/member/ChangePwServlet_MP.java
@@ -10,50 +10,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ChangePwServlet
- */
-@WebServlet("/changePwServlet")
-public class ChangePwServlet_Main extends HttpServlet {
+
+@WebServlet("/changePwServlet_MP")
+public class ChangePwServlet_MP extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		String mail = request.getParameter("mEmail");
+
+
+		HttpSession session = request.getSession();
+		int seq = (int)session.getAttribute("SESS_SEQ");
 		String npw = request.getParameter("newPw");
 		
-		System.out.println(mail+npw+"---------");
-		
+		System.out.println(npw+"=============");
 		
 		MemberDAO dao = new MemberDAO();
 		MemberVO mvo = new MemberVO();
 		
-		mvo.setmEmail(mail);
 		mvo.setmPw(npw);
-
+		mvo.setmSeq(seq);
 		
-		//분실변경 --
+		//MYPAGE변경 --
 		int res = 0;
-		res = dao.changePw(mvo);
-		
+		res = dao.changePw2(mvo);
 		
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		
-		//비밀번호 분실 후 변경-------------
+		
+		//MYPAGE에서 변경-----------------
 		if(res > 0) {
-			out.println("<script> alert('비밀번호가 변경되었습니다. 다시 로그인 해 주세요.'); location.href='/user/login.jsp';</script>");
-			
+			out.println("<script> alert('비밀번호가 변경되었습니다.'); location.href='/user/index.jsp';</script>");
 		} else {
-			out.println("<script> alert('입력된 비밀번호가 일치하지 않습니다. 다시 시도해 주세요.'); location.href='/pwChange_Main.jsp';</script>");
+			out.println("<script> alert('입력된 비밀번호가 일치하지 않습니다. 다시 시도해 주세요.'); location.href='/user/pwChange_Mypage.jsp';</script>");
 		}
-
+						
+		
+	
 	}
 
 }
